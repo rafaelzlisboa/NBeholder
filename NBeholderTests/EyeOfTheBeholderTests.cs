55a3dbe929e3ee5b9fe2a681d4e81a8c1cfffdc9
Pepato <rpepato@gmail.com>
2011-11-24 08:02:36 -0200

    Added a test to track a runtime loaded assembly

--- NBeholderTests/EyeOfTheBeholderTests.cs
+++ NBeholderTests/EyeOfTheBeholderTests.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Xml.Linq;
 using NBeholder;
 using NUnit.Framework;
@@ -45,6 +47,15 @@ namespace NBeholderTests
                 eyeXml.Elements("RunningAssemblies").Elements("RunningAssembly").Single(r => r.Element("AssemblyName").Value == "NBeholder");
 
             Assert.AreEqual(queryResult.Element("AssemblyName").Value, "NBeholder");
+        }
+
+        [Test]
+        public void EyeOfTheBeholder_ShouldFindSystemDataDll()
+        {
+            Assembly.Load("System.Data, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
+            EyeOfTheBeholder eye = new EyeOfTheBeholder();
+            var runningAssemblies = eye.RunningAssemblies();
+            Assert.True(runningAssemblies.Any(x => x["AssemblyName"].Equals("System.Data")));
         }
     }
 }
